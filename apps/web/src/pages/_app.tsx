24450cdf77f4f29472c4b1692394a1db8220cd2c
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2022-11-18 11:56:21 +0200

    wagmi

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -4,8 +4,43 @@ import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
 
+import { publicProvider } from "wagmi/providers/public";
+
+import { InjectedConnector } from "wagmi/connectors/injected";
+
+import { Session } from "next-auth";
+import {
+  configureChains,
+  defaultChains,
+  createClient,
+  WagmiConfig,
+} from "wagmi";
+
+const { chains, provider, webSocketProvider } = configureChains(defaultChains, [
+  publicProvider(),
+]);
+
+const client = createClient({
+  autoConnect: true,
+  connectors: [
+    new InjectedConnector({
+      chains,
+      options: {
+        name: "Injected",
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  provider,
+  webSocketProvider,
+});
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiConfig client={client}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
