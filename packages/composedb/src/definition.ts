2ce8ca942ee2ee5b72f6057aeab21b240e2680d0
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2022-11-30 15:33:55 +0200

    .

--- packages/composedb/src/definition.ts
+++ packages/composedb/src/definition.ts
@@ -1,3 +1,33 @@
 // This is an auto-generated file, do not edit manually
-import type { RuntimeCompositeDefinition } from '@composedb/types'
-export const definition: RuntimeCompositeDefinition = {"models":{"Thread":{"id":"kjzl6hvfrbw6c9aofa8penmje3t7ulzvrgdsm7mqiep6h2p7xps46717pl5vk73","accountRelation":{"type":"list"}},"Comment":{"id":"kjzl6hvfrbw6c9fi3m3qzse6jxkloa84tp4xqk2qp4031qqbhpeg890admje9bu","accountRelation":{"type":"list"}}},"objects":{"Thread":{"title":{"type":"string","required":true},"community":{"type":"string","required":true},"createdAt":{"type":"datetime","required":true},"author":{"type":"view","viewType":"documentAccount"}},"Comment":{"text":{"type":"string","required":true},"threadID":{"type":"streamid","required":true},"createdAt":{"type":"datetime","required":true},"author":{"type":"view","viewType":"documentAccount"}}},"enums":{},"accountData":{"threadList":{"type":"connection","name":"Thread"},"commentList":{"type":"connection","name":"Comment"}}}
+import type { RuntimeCompositeDefinition } from "@composedb/types";
+export const definition: RuntimeCompositeDefinition = {
+  models: {
+    Thread: {
+      id: "kjzl6hvfrbw6c9aofa8penmje3t7ulzvrgdsm7mqiep6h2p7xps46717pl5vk73",
+      accountRelation: { type: "list" },
+    },
+    Comment: {
+      id: "kjzl6hvfrbw6c9fi3m3qzse6jxkloa84tp4xqk2qp4031qqbhpeg890admje9bu",
+      accountRelation: { type: "list" },
+    },
+  },
+  objects: {
+    Thread: {
+      title: { type: "string", required: true },
+      community: { type: "string", required: true },
+      createdAt: { type: "datetime", required: true },
+      author: { type: "view", viewType: "documentAccount" },
+    },
+    Comment: {
+      text: { type: "string", required: true },
+      threadID: { type: "streamid", required: true },
+      createdAt: { type: "datetime", required: true },
+      author: { type: "view", viewType: "documentAccount" },
+    },
+  },
+  enums: {},
+  accountData: {
+    threadList: { type: "connection", name: "Thread" },
+    commentList: { type: "connection", name: "Comment" },
+  },
+};
