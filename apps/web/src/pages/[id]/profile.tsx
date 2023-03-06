0dbd6da5431f3de5b3828a89a5b02f7ad8ade333
ap-atul <atulpatare99@gmail.com>
2023-03-06 19:54:11 +0530

    refactored profile page with flex components

--- apps/web/src/pages/[id]/profile.tsx
+++ apps/web/src/pages/[id]/profile.tsx
@@ -8,6 +8,7 @@ import {useState} from "react";
 import {Tab} from "@headlessui/react";
 import ThreadCard from "../../components/ThreadCard";
 import * as utils from "../../utils";
+import {FlexColumn, FlexRow} from "../../components/Flex";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -22,25 +23,25 @@ const Profile = () => {
   let [categories] = useState({
     All: [
       {
-        id: 1,
+        id: '1',
         title: 'Does drinking coffee make you smarter?',
-        author: {id: 1},
+        author: {id: '1'},
         createdAt: Date.now(),
       },
     ],
     Questions: [
       {
-        id: 1,
+        id: '1',
         title: 'Is tech making coffee better or worse?',
-        author: {id: 1},
+        author: {id: '1'},
         createdAt: Date.now(),
       },
     ],
     Answers: [
       {
-        id: 1,
+        id: '1',
         title: 'Ask Me Anything: 10 answers to your questions about coffee',
-        author: {id: 1},
+        author: {id: '1'},
         createdAt: Date.now(),
       },
     ],
@@ -53,26 +54,24 @@ const Profile = () => {
     >
       <div className="mt-14">
         <Back link={"/"}/>
-        <div className="mt-6 flex flex-row justify-between items-center">
-          <div className="flex flex-row items-center">
+        <FlexRow classes="mt-6  justify-between">
+          <FlexRow>
             <AvatarCard image={undefined} imageSize={76}/>
-            <div className="flex flex-col ml-7 gap-2">
+            <FlexColumn classes="ml-7 gap-2">
               <span className="text-xl">Atul Patare</span>
               <Badge
                 text={utils.formatWalletAddress("0x7c98C2DEc5038f00A2cbe8b7A64089f9c0b51991")}
-                onClick={() => {}}
+                onClick={() => {
+                }}
               />
-            </div>
-          </div>
-          <div className="flex flex-row items-center">
-            <div className="flex flex-col mr-7">
-              <span>followers</span>
-              <span>30,003</span>
-            </div>
-            <AccentButton classes={"float-right"} title={"follow"} onClick={() => {
+            </FlexColumn>
+          </FlexRow>
+          <FlexRow>
+            <span>followers <br></br> 30,003</span>
+            <AccentButton classes={"float-right ml-7"} title={"follow"} onClick={() => {
             }}/>
-          </div>
-        </div>
+          </FlexRow>
+        </FlexRow>
       </div>
 
       <div className="w-full px-2 py-16 sm:px-0">
@@ -85,8 +84,8 @@ const Profile = () => {
                   selected
                     ? 'border-b-2 border-black rounded-t-lg'
                     : ''
-                )
-              }>
+                )}
+              >
                 {name}
               </Tab>
             ))}
