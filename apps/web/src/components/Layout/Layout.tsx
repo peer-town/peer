cc13d5042da0c6ca5623ffb8924f84b9c8a58234
ap-atul <atulpatare99@gmail.com>
2023-03-13 17:18:31 +0530

    added community list to layout with dummy content

--- apps/web/src/components/Layout/Layout.tsx
+++ apps/web/src/components/Layout/Layout.tsx
@@ -1,15 +1,23 @@
-import { NavBar } from "../NavBar";
+import {NavBar} from "../NavBar";
+import {CommunityAvatar} from "../CommunityAvatar";
 
 const Layout = (props) => {
 
   return (
     <div className="flex min-h-screen flex-col">
       <NavBar
-        handleDiscordUser = {props.handleDiscordUser}
-        handleDidSession = {props.handleDidSession}
-       />
-      <div className="relative mx-auto w-full max-w-7xl grow px-5 lg:px-0">
-        {props.children}
+        handleDiscordUser={props.handleDiscordUser}
+        handleDidSession={props.handleDidSession}
+      />
+      <div className="flex flex-row">
+        <div className="flex flex-col gap-7 px-6 py-10 border-r h-screen sm:hidden md:flex">
+          {Array(10).fill(0).map((_, index) => (
+            <CommunityAvatar key={index} name={"Devnode"} image={"https://placekitten.com/200/200"}/>
+          ))}
+        </div>
+        <div className="relative mx-auto w-full grow px-5 lg:px-0">
+          {props.children}
+        </div>
       </div>
     </div>
   );
