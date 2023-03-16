e896217a44ba9cff0889d039fec0a3dce0110696
rushikesh dhanwant <rushikeshdhanwant@gmail.com>
2023-03-16 13:19:43 +0530

    fixed community list panel ui and added create community button

--- apps/web/src/components/Layout/Layout.tsx
+++ apps/web/src/components/Layout/Layout.tsx
@@ -1,33 +1,45 @@
-import {NavBar} from "../NavBar";
-import {CommunityAvatar} from "../CommunityAvatar";
-import {trpc} from "../../utils/trpc";
+import { NavBar } from "../NavBar";
+import { CommunityAvatar } from "../CommunityAvatar";
+import { trpc } from "../../utils/trpc";
 import * as utils from "../../utils";
-import {has, get, isNil, isEmpty} from "lodash";
-import {selectCommunity, useAppDispatch, useAppSelector} from "../../store";
+import { has, get, isNil, isEmpty } from "lodash";
+import { selectCommunity, useAppDispatch, useAppSelector } from "../../store";
+import { CommunityOnBoardModal } from "../Modal";
+import { useState } from "react";
 
 const Layout = (props) => {
   const dispatch = useAppDispatch();
-  const communityId = useAppSelector(state => state.community.selectedCommunity);
+  const communityId = useAppSelector(
+    (state) => state.community.selectedCommunity
+  );
   const communities = trpc.public.fetchAllCommunities.useQuery();
+
+  const [communityOnboarding, setCommunityOnboarding] =
+    useState<boolean>(false);
 
   const handleOnCommunityClick = (id: string) => {
     dispatch(selectCommunity(id));
-  }
+  };
 
+  const handleCreateCommunity = () => {
+    setCommunityOnboarding(true);
+  };
   const getCommunityList = () => {
     if (isNil(communities.data) || isEmpty(communities.data)) {
       return <></>;
     }
 
     if (isEmpty(communityId) && has(communities, "data[0].node.id")) {
-        handleOnCommunityClick(get(communities, "data[0].node.id"));
+      handleOnCommunityClick(get(communities, "data[0].node.id"));
     }
 
     return communities.data.map((community, index) => {
-      if(!community.node) return <></>;
+      if (!community.node) return <></>;
 
       const name = community.node.communityName;
-      const image = get(community, "node.socialPlatforms.edges[0].node.communityAvatar") || "https://placekitten.com/200/200";
+      const image =
+        get(community, "node.socialPlatforms.edges[0].node.communityAvatar") ||
+        "https://placekitten.com/200/200";
       const selected = community.node.id == communityId;
       return (
         <CommunityAvatar
@@ -40,7 +52,7 @@ const Layout = (props) => {
         />
       );
     });
-  }
+  };
 
   return (
     <div className="flex min-h-screen flex-col">
@@ -49,22 +61,30 @@ const Layout = (props) => {
         handleDidSession={props.handleDidSession}
       />
       <div className="flex flex-row">
-        <div className="flex flex-col gap-7 py-10 border-r h-screen sm:hidden md:flex " >
-        <CommunityAvatar
-          classes={utils.classNames(
-            "bg-slate-300	w-full rounded-full hover:rounded-xl hover:bg-slate-500" 
-          )}
-          width={25}
-          name={"create community"}
-          image={"/plus.png"}
-          selected={false}
-          onClick={() => {}}
-        />
+        <div className="flex h-screen flex-col gap-7 border-r py-10 sm:hidden md:flex ">
+          <CommunityAvatar
+            classes={utils.classNames(
+              "bg-slate-300	w-full rounded-full hover:rounded-xl hover:bg-slate-500"
+            )}
+            width={25}
+            name={"create community"}
+            image={"/plus.png"}
+            selected={false}
+            onClick={() => {}}
+          />
           {getCommunityList()}
         </div>
         <div className="relative mx-auto w-full grow px-6">
           {props.children}
         </div>
+
+        <CommunityOnBoardModal
+          open={communityOnboarding}
+          onClose={() => {
+            setCommunityOnboarding(false);
+          }}
+          onSubmit={() => {}}
+        />
       </div>
     </div>
   );
