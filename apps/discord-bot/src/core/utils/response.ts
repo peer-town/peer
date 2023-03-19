2df892eafda67a94ae974b8fb466313bed812998
ap-atul <atulpatare99@gmail.com>
2023-03-19 22:15:27 +0530

    updated response util

--- apps/discord-bot/src/core/utils/response.ts
+++ apps/discord-bot/src/core/utils/response.ts
@@ -2,6 +2,7 @@ import {Response} from "express";
 
 export const Resp = {
   ok: (res: Response, msg: string) => res.status(200).json({msg}).end(),
+  okD: (res: Response, data: object, msg: string) => res.status(200).json({msg, data}).end(),
   notOk: (res: Response, msg: string) => res.status(401).json({msg}).end(),
   error: (res: Response, msg: string) => res.status(500).json({msg}).end(),
 }
