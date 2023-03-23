2b48d898332e464ef9aeceea58ec3974541073b0
rushikesh dhanwant <rushikeshdhanwant@gmail.com>
2023-03-23 20:46:08 +0530

    added api to update thread using social id

--- apps/web/src/server/trpc/router/thread.ts
+++ apps/web/src/server/trpc/router/thread.ts
@@ -1,6 +1,10 @@
 import { publicProcedure, router } from "../trpc";
 import { z } from "zod";
-import { composeMutationHandler, definition, ThreadInput } from "@devnode/composedb";
+import {
+  composeMutationHandler,
+  definition,
+  ThreadInput,
+} from "@devnode/composedb";
 import { ComposeClient } from "@composedb/client";
 import { config } from "../../../config";
 import { left, right } from "../../../utils/fp";
@@ -14,13 +18,19 @@ export const compose = new ComposeClient({
 
 const createThreadSchema = z.object({
   session: z.string(),
-  communityId: z.string(), 
-  userId: z.string(), 
-  threadId: z.string(), //social id
+  communityId: z.string(),
+  userId: z.string(),
+  threadId: z.string(),
   title: z.string(),
   body: z.string(),
-  createdFrom: z.string(), 
+  createdFrom: z.string(),
   createdAt: z.string(),
+});
+
+const updateThreadSocialId = z.object({
+  session: z.string(),
+  streamId: z.string(),
+  threadId: z.string(),
 });
 
 const getHandler = async (didSession: string) => {
@@ -37,6 +47,23 @@ export const threadRouter = router({
         const handler = await getHandler(input.session);
         const payload = omit(input, ["session"]);
         const response = await handler.createThread(payload as ThreadInput);
+        return response.errors && response.errors.length > 0
+          ? left(response.errors)
+          : right(response.data);
+      } catch (e) {
+        return left(e);
+      }
+    }),
+  updateThreadWithSocialId: publicProcedure
+    .input(updateThreadSocialId)
+    .mutation(async ({ input }) => {
+      try {
+        const handler = await getHandler(input.session);
+        const payload = omit(input, ["session"]);
+        const response = await handler.updateThreadWithSocialThreadId(
+          input.streamId,
+          input.threadId
+        );
         return response.errors && response.errors.length > 0
           ? left(response.errors)
           : right(response.data);
