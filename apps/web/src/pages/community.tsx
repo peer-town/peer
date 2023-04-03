a71b2f898768cee765086605db3ec5e1c774da02
ap-atul <atulpatare99@gmail.com>
2023-04-03 16:48:30 +0530

    added community name and search bar

--- apps/web/src/pages/community.tsx
+++ apps/web/src/pages/community.tsx
@@ -1,3 +1,4 @@
+import { get } from "lodash";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
@@ -5,18 +6,22 @@ import { Loader } from "../components/Loader";
 import { ThreadCard } from "../components/ThreadCard";
 import { ThreadSection } from "../components/ThreadSection";
 import { trpc } from "../utils/trpc";
+import { Search } from "../components/Search";
 
-const ThreadPage = () => {
+const CommunityPage = () => {
   const router = useRouter();
   const communityId = router.query.communityId as string;
   const threadId = router.query.threadId as string;
   const [currentThread, setCurrentThread] = useState<string>(threadId);
+
+  const community = trpc.community.fetchCommunityUsingStreamId.useQuery({streamId: communityId});
+  const communityName = get(community, "data.value.node.communityName");
   const threads = trpc.public.fetchAllCommunityThreads.useQuery({
     communityId,
   });
 
   useEffect(() => {
-    if(threadId) setCurrentThread(threadId);
+    if (threadId) setCurrentThread(threadId);
   }, [threadId]);
 
   useEffect(() => {
@@ -31,23 +36,27 @@ const ThreadPage = () => {
   }
 
   return (
-    <div className="flex flex-row">
-      <div className="scrollbar-hide mx-4 flex h-screen w-[40%] flex-col space-y-4 overflow-y-scroll pt-4">
-        {threads.data &&
-          threads.data.map((thread) => (
-            <Link
-              key={thread.node.id}
-              href={{
-                pathname: "/community",
-                query: {
-                  communityId,
-                  threadId: thread.node.id,
-                },
-              }}
-            >
-              <ThreadCard key={thread.node.id} thread={thread.node} />
-            </Link>
-          ))}
+    <div className="flex flex-row max-h-screen overflow-y-hidden">
+      <div className="w-[40%] mx-4 hidden md:block">
+        {communityName && <p className="text-4xl font-medium my-4">{communityName}</p>}
+        <Search onQuery={() => {}}/>
+        <div className="scrollbar-hide flex h-screen flex-col mt-4 space-y-4 overflow-y-scroll pt-4">
+          {threads.data &&
+            threads.data.map((thread) => (
+              <Link
+                key={thread.node.id}
+                href={{
+                  pathname: "/community",
+                  query: {
+                    communityId,
+                    threadId: thread.node.id,
+                  },
+                }}
+              >
+                <ThreadCard key={thread.node.id} thread={thread.node} />
+              </Link>
+            ))}
+        </div>
       </div>
       <div className="w-full border-l">
         {currentThread && <ThreadSection threadId={currentThread} />}
@@ -56,4 +65,4 @@ const ThreadPage = () => {
   );
 };
 
-export default ThreadPage;
+export default CommunityPage;
