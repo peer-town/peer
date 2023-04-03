9b2e4e0bc74b29e5f0f17178428e02fd25362626
rushikesh dhanwant <rushikeshdhanwant@gmail.com>
2023-04-03 20:11:40 +0530

    added description in communityOnBoard model

--- apps/web/src/components/Modal/CommunityOnBoardModal/CommunityOnBoardModal.tsx
+++ apps/web/src/components/Modal/CommunityOnBoardModal/CommunityOnBoardModal.tsx
@@ -1,37 +1,49 @@
-import {useState} from 'react'
-import {CommunityOnBoardProps} from "../types";
-import {BaseModal} from "../BaseModal/BaseModal";
+import { useState } from "react";
+import { CommunityOnBoardProps } from "../types";
+import { BaseModal } from "../BaseModal/BaseModal";
 
 export const CommunityOnBoardModal = (props: CommunityOnBoardProps) => {
   const [name, setName] = useState<string>();
   const [imageUrl, setImageUrl] = useState<string>();
   const [tags, setTags] = useState<string>();
+  const [description, setDescription] = useState<string>();
 
   const onSave = (event) => {
-    event.preventDefault()
-    props.onSubmit({name, imageUrl, tags});
-  }
+    event.preventDefault();
+    props.onSubmit({ name, description, imageUrl, tags });
+  };
 
   return (
-    <BaseModal open={props.open} title={"community details"} onClose={props.onClose}>
+    <BaseModal
+      open={props.open}
+      title={"community details"}
+      onClose={()=>{}}
+    >
       <form onSubmit={onSave}>
         <input
-          className="w-full mt-5 h-12 border-solid border-2 border-gray-200 rounded-md py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+          className="mt-5 h-12 w-full rounded-md border-2 border-solid border-gray-200 py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
           placeholder="community name"
           type="text"
           maxLength={100}
           onChange={(e) => setName(e.target.value)}
           required={true}
         />
+        <textarea
+          className="mt-5 h-28 w-full rounded-md border-2 border-solid border-gray-200 py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+          placeholder="community description"
+          maxLength={1000}
+          onChange={(e) => setDescription(e.target.value)}
+          required={true}
+        />
         <input
-          className="w-full mt-5 h-12 border-solid border-2 border-gray-200 rounded-md py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+          className="mt-5 h-12 w-full rounded-md border-2 border-solid border-gray-200 py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
           placeholder="image url"
           type="url"
           onChange={(e) => setImageUrl(e.target.value)}
           required={true}
         />
         <input
-          className="w-full mt-5 h-12 border-solid border-2 border-gray-200 rounded-md py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+          className="mt-5 h-12 w-full rounded-md border-2 border-solid border-gray-200 py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
           placeholder="tags"
           type="text"
           onChange={(e) => setTags(e.target.value)}
@@ -39,11 +51,11 @@ export const CommunityOnBoardModal = (props: CommunityOnBoardProps) => {
         />
         <button
           type="submit"
-          className="inline-flex mt-24 h-12 w-full leading-8 justify-center rounded-md border border-transparent bg-[#3478F6] px-4 py-2 text-sm font-medium text-white hover:bg-blue-400 focus:outline-none"
+          className="mt-24 inline-flex h-12 w-full justify-center rounded-md border border-transparent bg-[#3478F6] px-4 py-2 text-sm font-medium leading-8 text-white hover:bg-blue-400 focus:outline-none"
         >
           Save
         </button>
       </form>
     </BaseModal>
-  )
-}
+  );
+};
