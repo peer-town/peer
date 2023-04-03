2c9c4e10b6d390d063ea5707d44b194de148a444
ap-atul <atulpatare99@gmail.com>
2023-04-03 13:44:51 +0530

    updated avatar card component

--- apps/web/src/components/AvatarCard/AvatarCard.tsx
+++ apps/web/src/components/AvatarCard/AvatarCard.tsx
@@ -1,29 +1,35 @@
-import {AvatarCardProps} from "./types";
+import { AvatarCardProps } from "./types";
 import Image from "next/image";
-import {Badge} from "../Badge";
+import { Badge } from "../Badge";
 import * as utils from "../../utils";
-import Link from "next/link";
 
 export const AvatarCard = (props: AvatarCardProps) => {
   const handleAddressClick = () => {
     if (props.onAddressClick) {
       props.onAddressClick(props.address);
     }
-  }
+  };
 
   return (
-    <Link href={props.href || "#"}>
-      <div id="avatar-card" className={`flex flex-row items-center gap-3 ${props.classes || ''}`}>
-        <Image
-          width={props.imageSize}
-          height={props.imageSize}
-          className="rounded-full"
-          src={props.image || "https://placekitten.com/200/200"}
-          alt={`${props.name} avatar`}
+    <div
+      id="avatar-card"
+      onClick={props.onClick}
+      className={`flex flex-row items-center gap-3 ${props.classes || ""}`}
+    >
+      <Image
+        width={props.imageSize}
+        height={props.imageSize}
+        className="rounded-full"
+        src={props.image || "https://placekitten.com/200/200"}
+        alt={`${props.name} avatar`}
+      />
+      {props.name && <div>{props.name} </div>}
+      {props.address && (
+        <Badge
+          text={utils.formatWalletAddress(props.address)}
+          onClick={handleAddressClick}
         />
-        {props.name && <div>{props.name} </div>}
-        {props.address && <Badge text={utils.formatWalletAddress(props.address)} onClick={handleAddressClick}/>}
-      </div>
-    </Link>
-  )
-}
+      )}
+    </div>
+  );
+};
