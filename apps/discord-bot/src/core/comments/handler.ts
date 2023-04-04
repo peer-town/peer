fbcf144f81863e020dd70428cc0e3318d9503012
ap-atul <atulpatare99@gmail.com>
2023-04-04 20:39:23 +0530

    updated comment api | query changes | tests refactored

--- apps/discord-bot/src/core/comments/handler.ts
+++ apps/discord-bot/src/core/comments/handler.ts
@@ -6,23 +6,32 @@ import {Resp} from "../utils/response";
 import {Client} from "discord.js";
 import {commentHandler as discordCommentHandler} from "../../bots/discord";
 import {logger} from "../utils/logger";
-import {communityHasSocial, getSocialCommunityId} from "../utils/data";
+import {communityHasSocial, getSocialCommunityId, getSocialThreadId} from "../utils/data";
 
 export const postComment = async (clients: Clients, req: Request, res: Response) => {
-  const {commentId} = req.body;
-  logger.info('core', {body: req.body});
-  const comment: Node<Comment> = await clients.composeQuery().fetchCommentDetails(commentId);
-  const socials = _.get(comment, "node.thread.community.socialPlatforms.edges");
+  try {
+    logger.info('core', {body: req.body});
+    const {commentId} = req.body;
+    const comment: Node<Comment> = await clients.composeQuery().fetchCommentDetails(commentId);
+    const socials = _.get(comment, "node.thread.community.socialPlatforms.edges");
 
-  if (communityHasSocial(socials, constants.PLATFORM_DISCORD_NAME)) {
-    postCommentToDiscord(clients.discord, comment);
-  } else {
-    return Resp.notOk(res, "No discord for this community, bailing out!");
+    if (communityHasSocial(socials, constants.PLATFORM_DISCORD_NAME)) {
+      const commentId = await postCommentToDiscord(clients.discord, comment);
+      const response = [{
+        platformName: constants.PLATFORM_DISCORD_NAME,
+        commentId: commentId,
+      }];
+      return Resp.okD(res, response, "Created comment on socials");
+    } else {
+      return Resp.notOk(res, "No discord for this community, bailing out!");
+    }
+  } catch (e) {
+    logger.error('core', {e, body: req.body});
+    return Resp.error(res, "Server error occurred");
   }
-  return Resp.ok(res, "Posted to socials!");
 };
 
-export const postCommentToDiscord = (discordClient: Client, comment: Node<Comment>) => {
+export const postCommentToDiscord = async (discordClient: Client, comment: Node<Comment>) => {
   const text = _.get(comment, "node.text");
   const socials = _.get(comment, "node.thread.community.socialPlatforms.edges");
   const threadStreamId = _.get(comment, "node.threadId");
@@ -31,9 +40,11 @@ export const postCommentToDiscord = (discordClient: Client, comment: Node<Commen
   const userAvatar = _.get(comment, "node.user.userPlatforms[0].platformAvatar");
   const userProfileLink = config.devnodeWebsite.concat(`/${userId}/profile`);
   const serverId = getSocialCommunityId(socials, constants.PLATFORM_DISCORD_NAME);
-  const threadId = _.get(comment, "node.thread.threadId");
+  const threadId = getSocialThreadId(
+    _.get(comment, "node.thread.socialThreadIds"),
+    constants.PLATFORM_DISCORD_NAME
+  );
 
   const payload = {text, userName, serverId, threadId, threadStreamId, userAvatar, userProfileLink};
-  discordCommentHandler.postComment(discordClient, payload)
-    .catch((e) => logger.error('core', {payload, e}));
+  return await discordCommentHandler.postComment(discordClient, payload)
 }
--- apps/discord-bot/tests/functional/comments.spec.ts
+++ apps/discord-bot/tests/functional/comments.spec.ts
@@ -1,21 +1,20 @@
 import chai, {expect} from "../setup";
 import {initServer} from "../../src/core";
-import {Express} from "express";
-import {fakeComposeClient, fakeComposeQueryClient, fakeDiscordClient} from "../mock/fakes";
-import {config} from "../../src/config";
+import {config, constants} from "../../src/config";
+import * as sinon from "sinon";
+import {beforeEach} from "node:test";
+import {fakeComposeClient, fakeComposeQueryClient, fakeDiscordClient, sendStub} from "../mock/fakes";
 
 describe("comment api", () => {
-  let server: Express;
   const url = "/api/web-comment";
   const header = {'x-api-key': config.server.apiKey};
-
-  before(() => {
-    server = initServer({
-      discord: fakeDiscordClient,
-      compose: fakeComposeClient,
-      composeQuery: fakeComposeQueryClient,
-    });
+  const server = initServer({
+    discord: fakeDiscordClient,
+    compose: fakeComposeClient,
+    composeQuery: fakeComposeQueryClient,
   });
+
+  beforeEach(() => sinon.restore());
 
   it("should authenticate the api call", async () => {
     const res = await chai.request(server).post(url);
@@ -32,5 +31,13 @@ describe("comment api", () => {
   it("should response with 200 on sent message", async () => {
     const res = await chai.request(server).post(url).set(header).send({commentId: "123"});
     expect(res.status).to.eql(200);
+    expect(sendStub).to.be.callCount(1);
+    expect(res.body).to.eql({
+      msg: "Created comment on socials",
+      data: [{
+        platformName: constants.PLATFORM_DISCORD_NAME,
+        commentId: 1
+      }]
+    })
   });
 });
