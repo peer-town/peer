e863bbfd26579ccff8f2759436c93503eb5436e4
ap-atul <atulpatare99@gmail.com>
2023-04-05 21:28:21 +0530

    added no data indicator

---
+++ apps/web/src/components/NoData/NoData.tsx
@@ -0,0 +1,20 @@
+interface NoDataProps {
+  title: string;
+  description: string;
+}
+
+export const NoData = (props: NoDataProps) => {
+  return (
+    <div className="grid min-h-full place-items-center px-4 lg:px-8">
+      <div className="text-center">
+        <p className="text-2xl m-2 font-bold text-[#ff225b]">(❍ᴥ❍ʋ)</p>
+        <h1 className="text-2xl font-bold text-gray-900 sm:text-2xl">
+          {props.title}
+        </h1>
+        <p className="mt-1 text-sm leading-7 text-gray-600">
+          {props.description}
+        </p>
+      </div>
+    </div>
+  );
+};
