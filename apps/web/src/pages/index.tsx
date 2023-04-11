f3eea2339bc45d05d187173106c7d3133b23c597
rushikesh dhanwant <rushikeshdhanwant@gmail.com>
2023-04-12 00:34:25 +0530

    refetch newly created commnity

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,80 +1,92 @@
-import { type NextPage } from "next";
-import { trpc } from "../utils/trpc";
+import {type NextPage} from "next";
+import {trpc} from "../utils/trpc";
 import Link from "next/link";
-import { Search } from "../components/Search";
-import { CommunityCard } from "../components/CommunityCard";
+import {Search} from "../components/Search";
+import {CommunityCard} from "../components/CommunityCard";
+import {useEffect} from "react";
+import {useAppSelector} from "../store";
 
 const Home: NextPage = () => {
+  const newlyCreatedCommunity = useAppSelector((state) => state.community.newlyCreatedCommunity);
+
   // @ts-ignore
-  const { data, fetchNextPage } = trpc.public.fetchCommunities.useInfiniteQuery(
-    {
-      first: 20,
-      after: undefined,
-    },
-    {
-      getNextPageParam: (lastPage) => {
-        if (lastPage.pageInfo.hasNextPage) {
-          return {
-            cursor: {
-              after: lastPage.pageInfo.endCursor,
-              first: 1,
-            },
-          };
-        } else {
-          return null;
-        }
+  const {data, fetchNextPage, refetch} = trpc.public.fetchCommunities.useInfiniteQuery(
+      {
+        first: 20,
+        after: undefined,
       },
-    }
+      {
+        getNextPageParam: (lastPage) => {
+          if (lastPage.pageInfo.hasNextPage) {
+            return {
+              cursor: {
+                after: lastPage.pageInfo.endCursor,
+                first: 1,
+              },
+            };
+          } else {
+            return null;
+          }
+        },
+      }
   );
 
+  useEffect(() => {
+    const fetchData = async () => {
+      await refetch();
+    }
+    fetchData();
+  }, [newlyCreatedCommunity])
+
   return (
-    <div className="container mx-auto">
-      <h1 className="p-4 text-4xl font-medium">discover</h1>
-      <div className="mx-4 max-w-[682px]">
-        <Search label={"Search by name or tags"} onQuery={() => {}} />
-      </div>
-      <div className="m-4 mt-12 grid gap-8 md:grid-cols-1 lg:grid-cols-2">
-        {data &&
-          data.pages &&
-          data.pages.map((page) => {
-            return (
-              page &&
-              page.edges.map((community, index) => {
+      <div className="container mx-auto">
+        <h1 className="p-4 text-4xl font-medium">discover</h1>
+        <div className="mx-4 max-w-[682px]">
+          <Search label={"Search by name or tags"} onQuery={() => {
+          }}/>
+        </div>
+        <div className="m-4 mt-12 grid gap-8 md:grid-cols-1 lg:grid-cols-2">
+          {data &&
+              data.pages &&
+              data.pages.map((page) => {
                 return (
-                  <Link
-                    key={index}
-                    href={{
-                      pathname: "/community",
-                      query: { communityId: community.node.id },
-                    }}
-                  >
-                    <CommunityCard
-                      key={index}
-                      communityName={community.node.communityName}
-                      about={community.node.description}
-                      communityAvatar={
-                        community.node.socialPlatforms.edges[0]?.node
-                          .communityAvatar
-                      }
-                      members={20}
-                      questions={10}
-                      tags={[
-                        "solidity",
-                        "finance",
-                        "Next.js",
-                        "Another",
-                        "One",
-                        "More",
-                        "Two",
-                      ]}
-                    />
-                  </Link>
+                    page &&
+                    page.edges.map((community, index) => {
+                      return (
+                          <Link
+                              key={index}
+                              href={{
+                                pathname: "/community",
+                                query: {communityId: community.node.id},
+                              }}
+                          >
+                            <CommunityCard
+                                key={index}
+                                communityName={community.node.communityName}
+                                about={community.node.description}
+                                communityAvatar={
+                                  community.node.socialPlatforms.edges[0]?.node
+                                      .communityAvatar
+                                }
+                                members={20}
+                                questions={10}
+                                tags={[
+                                  "solidity",
+                                  "finance",
+                                  "Next.js",
+                                  "Another",
+                                  "One",
+                                  "More",
+                                  "Two",
+                                ]}
+                            />
+                          </Link>
+                      );
+                    })
                 );
-              })
-            );
-          })}
+              })}
+        </div>
       </div>
-    </div>
   );
 };
 
