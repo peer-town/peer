739d1923b423e5905c5b042bfe4a7ff6b7d71e1f
rushikesh dhanwant <rushikeshdhanwant@gmail.com>
2023-04-12 11:04:05 +0530

    updated background color

--- apps/web/src/components/ThreadSection/ThreadSection.tsx
+++ apps/web/src/components/ThreadSection/ThreadSection.tsx
@@ -76,7 +76,7 @@ export const ThreadSection = (props: ThreadSectionProps) => {
         </div>
       </div>
       <div className="absolute bottom-0 pb-[20px] h-auto bg-[#FBFBFB] mt-4 w-[70%]">
-      <div className="flex flex-row py-2 px-4 rounded-xl border h-auto items-center">
+      <div className="flex flex-row py-2 px-4 rounded-xl bg-white border h-auto items-center">
         <textarea
           id="chat"
           ref={commentBoxRef}
