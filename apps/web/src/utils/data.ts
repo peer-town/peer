8251422a4f39aac32d4060fc0ddc23d59deb558b
ap-atul <atulpatare99@gmail.com>
2023-04-14 20:49:13 +0530

    added filled svg to indicate action

--- apps/web/src/utils/data.ts
+++ apps/web/src/utils/data.ts
@@ -1,11 +1,26 @@
 import {Edges, Vote, Node} from "@devnode/composedb";
-import {isEmpty, isNil} from "lodash";
+import {isEmpty, isNil, get} from "lodash";
 
 export const getAbsVotes = (votes: Edges<Vote> | undefined): number => {
   if (isNil(votes) || isEmpty(votes.edges)) return 0;
   const upVotesCount = getUpVotes(votes.edges);
   const downVotesCount = getDownVotes(votes.edges);
   return upVotesCount - downVotesCount;
+}
+
+export const getUserVote = (votes: Edges<Vote> | undefined, userId: string | undefined) => {
+  if (isNil(votes) || isEmpty(votes.edges) || isNil(userId)) return undefined;
+  return votes.edges.find((vote) => vote.node.userId === userId);
+}
+
+export const isUpVoted = (vote: Node<Vote> | undefined): boolean => {
+  if (isNil(vote) || isNil(get(vote, "node.vote"))) return false;
+  return vote.node.vote;
+}
+
+export const isDownVoted = (vote: Node<Vote> | undefined): boolean => {
+  if (isNil(vote) || isNil(get(vote, "node.vote"))) return false;
+  return !vote.node.vote;
 }
 
 const getUpVotes = (votes: Node<Vote>[]): number => {
