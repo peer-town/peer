21a1ebb141898739114dafb9a75d40481f8fb6b5
rushikesh dhanwant <rushikeshdhanwant@gmail.com>
2023-04-25 16:00:26 +0530

    updated break points

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -16,10 +16,11 @@ module.exports = {
       'tablet': {'min':'520px','max':'1001px'},
       'mobile': {'max':'521px'},
       '860-1001': {'min':'860px','max':'1001px'},
-      '521-861': {'min':'521px','max':'861px'},
+      '580-861': {'min':'580px','max':'861px'},
       'max-680': {'max':'680px'},
       'min-680': {'min':'680px'},
       'min-500': {'min':'500px'},
+      'max-580': {'max':'580px'},
       'min-1001': {'min':'1001px'},
       'max-1000': {'max':'1000px'},
       // => @media (min-width: 1536px) { ... }
