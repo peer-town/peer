17907d29119f87a3a6e7ce462a17d5eb044ad3d1
ap-atul <atulpatare99@gmail.com>
2023-05-05 12:24:27 +0530

    added initial radicle setup

---
+++ apps/radicle/install.sh
@@ -0,0 +1,19 @@
+echo "Installing rust"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+echo "Installing radicle tools"
+cargo install --force --locked --git https://seed.radicle.xyz/z3gqcJUoA1n9HaHKufZs5FCSGazv5.git \
+    radicle-cli radicle-node radicle-httpd radicle-remote-helper
+
+echo "Setting rad pass phrase"
+export RAD_PASSPHRASE=some-secret
+
+echo "Running radicle node"
+radicle-node &
+radicle-httd &
+
+echo "Setting up rad"
+rad auth
+rad self
+
+echo "Done"
